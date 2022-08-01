502b0eb2f11e8cb7cfdd7057bdd65a555fd06009
Moshe <mobgadol@gmail.com>
2022-08-01 19:04:37 -0400

    added CRUD methods for product

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -46,7 +46,6 @@ router.post("/", (req, res) => {
     product_name: req.body.product_name,
     price: req.body.price,
     stock: req.body.stock,
-    tagIds: req.body.tagIds,
   })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
